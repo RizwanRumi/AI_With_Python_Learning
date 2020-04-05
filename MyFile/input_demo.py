afdb8a6fc76af9fbf50c1bcaad05273f591b7b7e
rizwanrumi <rizwan.cse33@gmail.com>
2020-04-05 22:40:42 +0200

    updated input_demo file

--- MyFile/input_demo.py
+++ MyFile/input_demo.py
@@ -1,2 +1,23 @@
+'''
 name: str = input('Enter a name: ')
 print('Hello', name.title())
+
+num = float(input('Enter a number: '))
+num += 20
+print(num)
+
+x = eval(input('Enter an expression: '))
+print(x)
+
+'''
+
+names = input("Enter names separated by commas: ").title().split(",")
+assignments = input("Enter assignment counts separated by commas: ").split(",")
+grades = input("Enter grades separated by commas: ").split(",")
+
+message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
+submit before you can graduate. You're current grade is {} and can increase \
+to {} if you submit all assignments before the due date.\n\n"
+
+for name, assignment, grade in zip(names, assignments, grades):
+    print(message.format(name, assignment, grade, int(grade) + int(assignment)*2))
