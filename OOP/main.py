917ff65dcd9668b819920d2a8167ce714772b9cf
rizwanrumi <rizwan.cse33@gmail.com>
2020-04-09 00:39:40 +0200

    modified main file

--- OOP/main.py
+++ OOP/main.py
@@ -6,3 +6,22 @@ print(new_shirt.color)
 print(new_shirt.size)
 print(new_shirt.style)
 print(new_shirt.price)
+
+new_shirt.change_price(10)
+print('Price: {}'.format(new_shirt.price))
+
+print('Discount: {}'.format(new_shirt.discount(.2)))
+
+tshirt_collection =[]
+shirt_one = Shirt('orange', 'M', 'short sleeve', 25)
+shirt_two = Shirt('red', 'S', 'short sleeve', 15)
+shirt_three = Shirt('purple', 'XL', 'short sleeve', 10)
+
+tshirt_collection.append(shirt_one)
+tshirt_collection.append(shirt_two)
+tshirt_collection.append(shirt_three)
+
+for i in range(len(tshirt_collection)):
+    print('Tshirt color: ' + tshirt_collection[i].color)
+
+
