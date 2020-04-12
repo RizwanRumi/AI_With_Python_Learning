c78a84bbeec2e1aae0a4cdea82329764c5fd56ed
rizwanrumi <rizwan.cse33@gmail.com>
2020-04-12 11:57:29 +0200

    modified numpy files

--- Numpy_Practice/test_numpy.py
+++ Numpy_Practice/test_numpy.py
@@ -21,7 +21,7 @@ print(' total size: ', y.size)
 print(' obj type ', type(y))
 print('type : ', y.dtype)
 
-x = np.array([1.5, 2.2, 3.7, 4.0, 5.9], dtype = np.int64)
+x = np.array([[1.5, 2.2, 3.7, 4.0, 5.9],[9,8,7,6,5]], dtype = np.int64)
 
 print()
 print(x)
@@ -30,3 +30,11 @@ print(x.dtype)
 
 # save x into the current directory as
 np.save('my_numpy_array', x)
+
+# load the saved array from our current directory into variable y
+y = np.load('my_numpy_array.npy')
+
+# print information about the ndarray we loaded
+print(y)
+print('y is an object of type:', type(y))
+print('The elements in y are of type:', y.dtype)
