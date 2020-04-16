0a0ee0167abb705f416f3821b2131fb2ef580ca1
rizwanrumi <rizwan.cse33@gmail.com>
2020-04-17 00:21:03 +0200

    add arithmatic operation pandas file

---
+++ Pandas_Practice/arithmatic_operation_pandas.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+
+fruits = pd.Series(data=[10,6,3], index=['apples', 'oranges', 'bananas'])
+
+# We print fruits for reference
+print('Original grocery list of fruits:\n ', fruits)
+
+# We perform basic element-wise operations using arithmetic symbols
+print()
+print('fruits + 2:\n', fruits + 2) # We add 2 to each item in fruits
+print()
+print('fruits - 2:\n', fruits - 2) # We subtract 2 to each item in fruits
+print()
+print('fruits * 2:\n', fruits * 2) # We multiply each item in fruits by 2
+print()
+print('fruits / 2:\n', fruits / 2) # We divide each item in fruits by 2
+print()
+
+# use numpy
+print('EXP(X) = \n', np.exp(fruits))
+print()
+print('SQRT(X) =\n', np.sqrt(fruits))
+print()
+print('POW(X,2) =\n',np.power(fruits,2)) # We raise all elements of fruits to the power of 2
+
+# arithmetic operations on selected items
+
+print('Amount of Bananas + 2 = ', fruits['bananas'] + 2)
+print()
+
+print('Amount of apples - 2 = ', fruits.iloc[0] - 2)
+print()
+
+print('Amount of oranges + 4 = ', fruits.loc['oranges'] + 4)
+
+#important accessing elements
+print(fruits.loc['oranges'])
+print(fruits.loc[['oranges']])
+print(fruits.iloc[0])
+print(fruits.iloc[[0]])
